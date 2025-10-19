f08ff91197d5093483081b150d3b06c9df4c7014
montasirmoyen <montasirmoyen@gmail.com>
2025-10-18 23:39:11 -0400

    Bypass @typescript-eslint/no-explicit-any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
@@ -12,6 +13,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
