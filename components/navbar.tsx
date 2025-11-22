f253d4880890d5dec316842b14ad2c254c0641b2
montasirmoyen <montasirmoyen@gmail.com>
2025-11-21 19:42:31 -0500

    Improve navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,27 +2,18 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-<nav className="bg-white/50 backdrop-blur-md px-6 py-4 flex justify-center items-center">
+    <nav className="bg-white/50 backdrop-blur-md px-4 lg:px-6 py-4 flex justify-center items-center sticky top-0 z-20 shadow-sm">
       <div className="text-center">
-  <Link
-    href="/"
-    className="text-2xl font-bold text-gray-800 hover:text-red-300 transition"
-  >
-    Fragrance Catalog
-  </Link>
-  <p className="text-gray-600 text-sm mt-1">
-    Browse through the most popular fragrances
-  </p>
-</div>
+        <Link
+          href="/"
+          className="text-xl lg:text-2xl font-bold text-gray-800 hover:text-red-500 transition-colors"
+        >
+          Fragrance Catalog
+        </Link>
+        <p className="text-gray-600 text-xs lg:text-sm mt-1">
+          Browse through the most popular fragrances
+        </p>
+      </div>
     </nav>
   );
 }
-
-/*
- <Link href="/about" className="text-gray-700 hover:text-blue-600 transition">
-          About
-        </Link>
-        <Link href="/contact" className="text-gray-700 hover:text-blue-600 transition">
-          Contact
-        </Link>
- */
